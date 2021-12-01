95f6263fcc8245d9a388cf3cac3468cd2f6f0e96
ge-wijayanto <greg.evan01@gmail.com>
2021-12-01 16:03:08 +0700

    logic

---
+++ src/rot13.js
@@ -0,0 +1,22 @@
+function rot13(str, shift) {
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        let ch = str[i];
+
+        if (ch.toUpperCase(str[i])) {
+            let chTemp = String.fromCharCode((ch.charCodeAt(0) + shift-65) % 26 + 65);
+            result += chTemp;
+        } else {
+            let chTemp = String.fromCharCode((ch.charCodeAt(0) + shift-97) % 26 + 97);
+            result += chTemp;
+        }
+    }
+
+    console.log("Result : " + result);
+}
+
+let str = "PITON";
+let shift = 13;
+
+rot13(str, shift);
---
+++ src/vigenere.js
@@ -0,0 +1,62 @@
+function getKey(str, key) {
+    key = key.split("");
+
+    if (str.length == key.length) 
+        return key.join("");
+    else {
+        let temp = key.length;
+        for (let i = 0; i < (str.length-temp); i++)
+                key.push(key[i % ((key).length)]);
+    }
+} 
+
+function encryption(str, key) {
+    let cipher_text = "";
+
+    for (let i = 0; i < str.length; i++) {
+        // Konversi Range 0-25
+        let x = (str[i].charCodeAt(0) + key[i].charCodeAt(0)) % 26;
+
+        // Konversi ke alphabet
+        x += 'A'.charCodeAt(0);
+
+        cipher_text += String.fromCharCode(x);
+    }
+
+    console.log("Ciphertext : " + cipher_text);
+}
+
+function decryption(str, key) {
+    let plain_text = "";
+
+    for (let i = 0; i < str.length; i++) {
+        // Konversi Range 0-25
+        let x = (str[i].charCodeAt(0) - key[i].charCodeAt(0) + 26) % 26;
+
+        // Konversi ke alphabet
+        x += 'A'.charCodeAt(0);
+
+        plain_text += String.fromCharCode(x);
+    }
+
+    console.log("Plaintext : " + plain_text);
+}
+
+function toUpper(strtemp) {
+    let str = (strtemp).split("");
+
+    for (let i = 0; i < strtemp.length; i++)
+        if (strtemp[i] == strtemp[i].toLowerCase())
+            str[i] = strtemp[i].toUpperCase();
+    
+    strtemp = str.toString();
+    return strtemp;
+}
+
+
+let str; 
+let keyword;
+let key = getKey(str, keyword);
+ 
+encryption(str, key);
+decryption(str, key);
