c0790f25330ea1bb6ffe4b79a23132d3302227ea
mluthfit <mluthfitaufiq@gmail.com>
2021-12-03 10:58:16 +0700

    rebuild main.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,30 +5,41 @@ document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('button');
   buttons.forEach(button => {
     button.addEventListener('click', function() {
-      const text = document.querySelector('#input_text').value;
-      if (text !== '')  {
-        if (this.getAttribute('name') === 'vigenere') {
-          const key = document.querySelector('#input_key').value
-          if (key !== '') {
-            if (this.getAttribute('id') === 'encryption') {
-              generateResult(Vigenere.encryption(text, key));
-            } else {
-              generateResult(Vigenere.decryption(text, key));
-            }
-          }
-        } else if (this.getAttribute('name') === 'rot13') {
-          generateResult(ROT13.job(text));
+      const textInput = document.querySelector('#input_text') || false;
+      const keyInput = document.querySelector('#input_key') || false;
+      const textValue = textInput ? textInput.value : '';
+      const keyValue = keyInput ? keyInput.value : '';
+
+      if (!textInput || !textValue) {
+        clearResult();
+        return;
+      }
+
+      if (this.getAttribute('name') === 'vigenere') {
+        if (!keyInput || !keyValue || !isAllAlphabets(keyValue)) {
+          clearResult();
+          return;
+        }
+
+        if (this.getAttribute('id') === 'encryption') {
+          generateResult(Vigenere.encryption(textInput.value, keyInput.value));
         } else {
-          const key = document.querySelector('#input_key').value
-          if (key !== '' && isAllAlphabets(key)) {
-            if (this.getAttribute('id') === 'encryption') {
-              const vigenere = Vigenere.encryption(text, key);
-              generateResult(ROT13.job(vigenere));
-            } else {
-              const vigenere = Vigenere.decryption(text, key);
-              generateResult(ROT13.job(vigenere));
-            }
-          }
+          generateResult(Vigenere.decryption(textInput.value, keyInput.value));
+        }
+      } else if (this.getAttribute('name') === 'rot13') {
+        generateResult(ROT13.job(textInput.value));
+      } else {
+        if (!keyInput || !keyValue || !isAllAlphabets(keyValue)) {
+          clearResult();
+          return;
+        }
+
+        if (this.getAttribute('id') === 'encryption') {
+          const vigenere = Vigenere.encryption(textInput.value, keyInput.value);
+          generateResult(ROT13.job(vigenere));
+        } else {
+          const rot13 = ROT13.job(textInput.value);
+          generateResult(Vigenere.decryption(rot13, keyInput.value))
         }
       }
     });
@@ -53,4 +64,9 @@ const isAllAlphabets = (str) => {
 
 const checkAplhabet = (char) => {
   return (/[a-zA-Z]/).test(char);
+}
+
+const clearResult = () => {
+  const result = document.querySelector('.result');
+  result.innerHTML = ``;
 }
